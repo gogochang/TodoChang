ffef4f7bb37f294441c68687878e1c8d62bb797f
gogochang <okpo2581@gmail.com>
2023-01-09 20:19:35 +0900

    사이드메뉴 -> 계정설정에 각 버튼의 Alert 알림창 추가   - Alert 알림창 추가 Yes 선택시에만 해당 동작하도록 수정

--- myTodoApp/ViewController/AccountSettingVC.swift
+++ myTodoApp/ViewController/AccountSettingVC.swift
@@ -47,14 +47,22 @@ class AccountSettingVC: UIViewController {
     //MARK: - 로그아웃 클릭 함수
     @IBAction func logOutButtonClicked(_ sender: UIButton) {
         print("AccountSettingVC - logOutButtonClicked() called")
+        let alret = UIAlertController(title: "로그아웃", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
+        let no = UIAlertAction(title: "No", style: .destructive, handler: nil)
+        let yes = UIAlertAction(title: "Yes", style: .default) { [weak self] UIAlertAction in
+            guard let self = self else { return }
+            let storyboard = UIStoryboard.init(name: "Login", bundle: nil)
+            let LoginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC")
+            
+            UserDefaults.standard.removeObject(forKey: "ID")
+            UserDefaults.standard.removeObject(forKey: "PASSWORD")
+            
+            self.changeRootViewController(LoginVC)
+        }
         
-        let storyboard = UIStoryboard.init(name: "Login", bundle: nil)
-        let LoginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC")
-        
-        UserDefaults.standard.removeObject(forKey: "ID")
-        UserDefaults.standard.removeObject(forKey: "PASSWORD")
-        
-        changeRootViewController(LoginVC)
+        alret.addAction(yes)
+        alret.addAction(no)
+        present(alret, animated: true, completion: nil)
     }
     
     //MARK: - 계정 삭제 버튼
@@ -63,32 +71,38 @@ class AccountSettingVC: UIViewController {
         guard let id = idNum else {
             print("ID Number is nil")
             return }
+        let alret = UIAlertController(title: "계정 삭제", message: "계정을 삭제하시겠습니까?", preferredStyle: .alert)
+        let no = UIAlertAction(title: "No", style: .destructive, handler: nil)
+        let yes = UIAlertAction(title: "Yes", style: .default) { UIAlertAction in
+            accountService.shared.deleteUsers(id: id, completion: { [weak self] (response) in
+                switch(response) {
+                case .success:
+                    print("success delete user")
+
+                    guard let self = self else { return }
+                    let storyboard = UIStoryboard.init(name: "Login", bundle: nil)
+                    let LoginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC")
+
+                    UserDefaults.standard.removeObject(forKey: "ID")
+                    UserDefaults.standard.removeObject(forKey: "PASSWORD")
+
+                    self.changeRootViewController(LoginVC)
+
+                case .requestErr(let message):
+                    print("requestErr", message)
+                case .pathErr:
+                    print("pathErr put")
+                case .serverErr:
+                    print("serverErr")
+                case .networkFail:
+                    print("networkFail")
+                }
+            })
+        }
         
-        //TODO: 계정 삭제
-        accountService.shared.deleteUsers(id: id, completion: { [weak self] (response) in
-            switch(response) {
-            case .success:
-                print("success delete user")
-                
-                guard let self = self else { return }
-                let storyboard = UIStoryboard.init(name: "Login", bundle: nil)
-                let LoginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC")
-                
-                UserDefaults.standard.removeObject(forKey: "ID")
-                UserDefaults.standard.removeObject(forKey: "PASSWORD")
-                
-                self.changeRootViewController(LoginVC)
-                
-            case .requestErr(let message):
-                print("requestErr", message)
-            case .pathErr:
-                print("pathErr put")
-            case .serverErr:
-                print("serverErr")
-            case .networkFail:
-                print("networkFail")
-            }
-        })
+        alret.addAction(yes)
+        alret.addAction(no)
+        present(alret, animated: true, completion: nil)
     }
     
     // Rootview 변경함수 코드정리필요
